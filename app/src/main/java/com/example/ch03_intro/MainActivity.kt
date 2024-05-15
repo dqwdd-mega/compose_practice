9e8210dbe7f525aa402a4d9fe0dfe4e9465812cc
dqwdd <dqwdd@naver.com>
2024-05-15 21:10:57 +0900

    시작 전 커밋

--- app/src/main/java/com/example/ch03_intro/MainActivity.kt
+++ app/src/main/java/com/example/ch03_intro/MainActivity.kt
@@ -5,28 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.ColorPainter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
 import com.example.ch03_intro.ui.theme.Ch03IntroTheme
 
 class MainActivity : ComponentActivity() {
@@ -41,83 +27,37 @@ class MainActivity : ComponentActivity() {
                 ) {
 //                    Greeting("zzzzzzzzzzz")
                     Column {
-                        CardEx(cardData)
-                        CardEx(cardData)
+//                        CheckBoxEx(cardData)
                     }
                 }
             }
         }
     }
 
-    companion object {
-        val cardData = CardData(
-            imageUri = "iVBORw0KGgoAAAANSUhEUgAAAAIAAAACCAYAAABytg0kAAAADUlEQVR42mP8/wcAAwAB/lnuNGwAAAAASUVORK5CYII=\n",
-            imageDescription = "Go양이",
-            author = "The Cat",
-            description = "Go Yang yee"
-        )
-    }
 }
 
 @Composable
-fun CardEx(cardData: CardData) {
-    val placeHolderColor = Color(0x33000000)
-
-    Card(
-        elevation = CardDefaults.cardElevation(8.dp),
-        modifier = Modifier.padding(4.dp)
+fun CheckBoxEx() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.padding(8.dp)
-        ) {
-            // Step 1
-            // 이미지 넣고, 텍스트도 2개 넣고 간격도 넣는 예제
-//            AsyncImage(
-//                model = R.drawable.ic_launcher_background, // cardData.imageUri
-//                contentDescription = cardData.imageDescription,
-//                modifier = Modifier.size(32.dp)
-//            )
-//            Spacer(
-//                modifier = Modifier.size(8.dp),
-//            )
-//            Column {
-//                Text(
-//                    text = cardData.author
-//                )
-//                Spacer(
-//                    modifier = Modifier.size(4.dp)
-//                )
-//                Text(
-//                    text = cardData.description
-//                )
-//            }
+        // Step 1: Create Checkbox - checked 속성은 false
+        // onCheckedChange 는 비워둡시다
 
-            // Step 2 - Step 1 의 이미지 둥글게
-            AsyncImage(
-                model = R.drawable.ic_launcher_background,
-                contentScale = ContentScale.Crop,
-                contentDescription = cardData.imageDescription,
-                placeholder = ColorPainter(placeHolderColor),
-                modifier = Modifier.size(32.dp)
-                    .clip(RoundedCornerShape(5.dp))
-            )
-            Spacer(
-                modifier = Modifier.size(8.dp),
-            )
-            Column {
-                Text(
-                    text = cardData.author
-                )
-                Spacer(
-                    modifier = Modifier.size(4.dp)
-                )
-                Text(
-                    text = cardData.description
-                )
-            }
+        // Step 2: onCheckedChange 에서 boolean 값 변수를 바꾸고
+        // checked 에서 그 값을 반영해봅시다(잘 되지 않습니다)
 
-        }
+        // Step 3: boolean 대신 remember { mutableStateOf(false) }를
+        // 사용하여 상태를 도입합시다(value 프로퍼티를 이용해야 합니다)
+
+        // Step 4: delegated properties 로 변경해봅시다
+
+        // Step 5: destruction 으로 상태를 받아서 사용해봅시다
+
+        // Checkbox 를 앞에 넣어주세요
+        Text(
+            text = "프로그래머입니까?"
+        )
     }
 }
 
@@ -126,6 +66,6 @@ fun CardEx(cardData: CardData) {
 @Composable
 fun GreetingPreview() {
     Row {
-        CardEx(MainActivity.cardData)
+        CheckBoxEx()
     }
 }
