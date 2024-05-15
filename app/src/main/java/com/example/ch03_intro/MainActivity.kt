f1baf64bff8670498cb3151d345047a1e76cd504
dqwdd <dqwdd@megastudy.net>
2024-05-15 22:47:31 +0900

    [Chapter03-13] : Step 3 Clear

--- app/src/main/java/com/example/ch03_intro/MainActivity.kt
+++ app/src/main/java/com/example/ch03_intro/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,7 +30,7 @@ class MainActivity : ComponentActivity() {
                 ) {
 //                    Greeting("zzzzzzzzzzz")
                     Column {
-//                        CheckBoxEx(cardData)
+                        CheckBoxEx()
                     }
                 }
             }
@@ -53,16 +55,23 @@ fun CheckBoxEx() {
 
         // Step 2: onCheckedChange 에서 boolean 값 변수를 바꾸고
         // checked 에서 그 값을 반영해봅시다(잘 되지 않습니다)
-        var checked = false
-        Checkbox(
-            checked = checked,
-            onCheckedChange = { // 바뀔 때 로직
-                checked = !checked
-            }
-        )
+//        var checked = false
+//        Checkbox(
+//            checked = checked,
+//            onCheckedChange = { // 바뀔 때 로직
+//                checked = !checked
+//            }
+//        )
 
         // Step 3: boolean 대신 remember { mutableStateOf(false) }를
         // 사용하여 상태를 도입합시다(value 프로퍼티를 이용해야 합니다)
+        val checked = remember { mutableStateOf(false) }
+        Checkbox(
+            checked = checked.value,
+            onCheckedChange = { // 바뀔 때 로직
+                checked.value = !checked.value
+            }
+        )
 
         // Step 4: delegated properties 로 변경해봅시다
 
