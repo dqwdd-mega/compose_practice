e975dae197841adcb6788af9235be8094c906f67
dqwdd <dqwdd@naver.com>
2024-05-15 18:17:49 +0900

    Step-2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,10 @@ android {
 
 dependencies {
 
+    // coil
+    implementation(libs.coil)
+    implementation(libs.coil.compose)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
