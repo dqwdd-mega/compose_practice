e306e1766a471df22032d722fdd47cfba54d4005
dqwdd-mega <dqwdd@megastudy.net>
2024-05-22 20:52:22 +0900

    [Chapter 03.16-Slot API] : Step 1 Setting

--- app/src/main/java/com/example/ch03_intro/MainActivity.kt
+++ app/src/main/java/com/example/ch03_intro/MainActivity.kt
@@ -5,22 +5,18 @@ package com.example.ch03_intro
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -35,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    TopBarEx("Android")
+                    SlotEx()
                 }
             }
         }
@@ -43,89 +39,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun TopBarEx(name: String) {
+fun SlotEx() {
+    val checked1 = remember { mutableStateOf(false) }
+    val checked2 = remember { mutableStateOf(false) }
+
     Column {
-        // Step 1: TopAppBar 를 만들고 title 항목을 채워봅시다
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Checkbox(
+                checked = checked1.value,
+                onCheckedChange = { checked1.value = it }
+            )
+            Text(
+                text = "텍스트-1",
+                modifier = Modifier.clickable { checked1.value = !checked1.value }
+            )
+        }
 
-        // Step 2: navigationIcon 파라미터를 채워봅시다
-        // IconButton 을 만들고 자식으로 Icon 을 넣읍시다
-        // 아이콘은 Icons.Filled.ArrowBack 을 채웁시다.
-        // onClick 은 비워둡시다.
-
-        // Step 3: actions 를 추가해봅시다.
-        // Icons.Filled 의 여러 아이콘을 이용해봅시다.
-        TopAppBar(
-            title = { Text(text = "TopAbbBar") },
-            navigationIcon = {
-                IconButton(onClick = {}) {
-                    Icon(
-                        imageVector = Icons.Filled.ArrowBack,
-                        contentDescription = "업 navigation"
-                    )
-                }
-            },
-            actions = {
-                IconButton(onClick = {}) {
-                    Icon(
-                        imageVector = Icons.Filled.Search,
-                        contentDescription = "검색"
-                    )
-                }
-                IconButton(onClick = {}) {
-                    Icon(
-                        imageVector = Icons.Filled.Settings,
-                        contentDescription = "설정"
-                    )
-                }
-                IconButton(onClick = {}) {
-                    Icon(
-                        imageVector = Icons.Filled.AccountBox,
-                        contentDescription = "계정"
-                    )
-                }
-            }
-        )
-
-        // Step 4: TopAppBar content 파라미터 버전을 만들어봅시다.
-        TopAppBar(
-            title = {
-                Text(
-                    text = "TopAppBar",
-                    modifier = Modifier.weight(1f)
-                ) },
-            navigationIcon = {
-                IconButton(onClick = {}) {
-                    Icon(
-                        imageVector = Icons.Filled.ArrowBack,
-                        contentDescription = "업 navigation"
-                    )
-                }
-            },
-            actions = {
-                IconButton(onClick = {}) {
-                    Icon(
-                        imageVector = Icons.Filled.Search,
-                        contentDescription = "검색"
-                    )
-                }
-                IconButton(onClick = {}) {
-                    Icon(
-                        imageVector = Icons.Filled.Settings,
-                        contentDescription = "설정"
-                    )
-                }
-                IconButton(onClick = {}) {
-                    Icon(
-                        imageVector = Icons.Filled.AccountBox,
-                        contentDescription = "계정"
-                    )
-                }
-            }
-        )
-
-        Text(
-            text = name
-        )
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Checkbox(
+                checked = checked2.value,
+                onCheckedChange = { checked2.value = it }
+            )
+            Text(
+                text = "텍스트-2",
+                modifier = Modifier.clickable { checked2.value = !checked2.value }
+            )
+        }
     }
 }
 
@@ -137,7 +80,7 @@ fun MyPreview() {
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
-            TopBarEx("Android")
+            SlotEx()
         }
     }
 }
