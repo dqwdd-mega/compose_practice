65542ad7082dbb0ac9a87a462787e69b12ba63b0
dqwdd-mega <dqwdd@megastudy.net>
2024-05-22 20:32:56 +0900

    [Chapter 03.15-TopAppBar] : Step 2 Clear

--- app/src/main/java/com/example/ch03_intro/MainActivity.kt
+++ app/src/main/java/com/example/ch03_intro/MainActivity.kt
@@ -8,7 +8,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -39,14 +46,22 @@ class MainActivity : ComponentActivity() {
 fun TopBarEx(name: String) {
     Column {
         // Step 1: TopAppBar 를 만들고 title 항목을 채워봅시다
-        TopAppBar(title = {
-            Text(text = "TopAbbBar")
-        })
 
         // Step 2: navigationIcon 파라미터를 채워봅시다
         // IconButton 을 만들고 자식으로 Icon 을 넣읍시다
         // 아이콘은 Icons.Filled.ArrowBack 을 채웁시다.
         // onClick 은 비워둡시다.
+        TopAppBar(
+            title = { Text(text = "TopAbbBar") },
+            navigationIcon = {
+                IconButton(onClick = {}) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = "업 navigation"
+                    )
+                }
+            }
+        )
 
         // Step 3: actions 를 추가해봅시다.
         // Icons.Filled 의 여러 아이콘을 이용해봅시다.
