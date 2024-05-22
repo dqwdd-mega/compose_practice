7e4804a2f38034d551b1b612240de72152d14a1e
dqwdd-mega <dqwdd@megastudy.net>
2024-05-22 21:27:13 +0900

    [Chapter 03.16-Slot API] : Step 3 Clear

--- app/src/main/java/com/example/ch03_intro/MainActivity.kt
+++ app/src/main/java/com/example/ch03_intro/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -69,10 +70,62 @@ class MainActivity : ComponentActivity() {
 /// 그리고 Slot API 를 만들 땐 보통 마지막 인자로 content 를 넣는 편이다
 /// 왜냐하면 마지막 인자로 만들어야 호출할 때 {} 이렇게 사용할 수 있기 때문(그게 보기 좋으니까)
 /// 람다에 @Composable 도 붙여줘야 한다
+//@Composable
+//fun CheckBoxWithSlotAPI(
+//    checked: MutableState<Boolean>,
+//    content: @Composable () -> Unit
+//) {
+//    Row(
+//        verticalAlignment = Alignment.CenterVertically,
+//        modifier = Modifier.clickable {
+//            checked.value = !checked.value
+//        }
+//    ) {
+//        Checkbox(
+//            checked = checked.value,
+//            onCheckedChange = { checked.value = it }
+//        )
+//        content()
+//    }
+//}
+
+// Step 3: 'content' 의 타입을 '@Composable RowScope.() -> Unit' 로 바꿉시다.
+// 이렇게 다른 콤포저블 컨텐트를 넣는 방식을 Slot API 라 합니다.
+
+/// Step 2에서의 코드도 괜찮았지만 더 개선할 부분이 있다.
+/// 여기선 그걸 개선한 코드를 보여준다.
+/// 1. Composable 부분
+/// content: @Composable () -> Unit  ----> content: @Composable RowScope.() -> Unit
+/// RowScope 로 바꾸는 게 좋다~(Column 을 원하면 ColumnScope)
+/// 왜 RowScope 로 예시를 들었냐면,
+/// 지금 감싸인 부분이 Row 니까 호출하는 부분에서도 RowScope 의 이점을 사용할 수 있게 하기 위해서이다
+/// 왜냐하면 람다 내에서 RowScope 안에 있는 것처럼 쓸 수 있다
+/// RowScope 안에 있는 것처럼 쓴다는 말은 이걸 호출하는 부분에서 modifier 를 쓸 수 있어서 그런 것 같다
+//@Composable
+//fun CheckBoxWithSlotAPI(
+//    checked: MutableState<Boolean>,
+//    content: @Composable RowScope.() -> Unit
+//) {
+//    Row(
+//        verticalAlignment = Alignment.CenterVertically,
+//        modifier = Modifier.clickable {
+//            checked.value = !checked.value
+//        }
+//    ) {
+//        Checkbox(
+//            checked = checked.value,
+//            onCheckedChange = { checked.value = it }
+//        )
+//        content()
+//    }
+//}
+
+// Step 4: 상태를 바꾸는 람다를 '@Composable' 함수의 인자로 뺍시다.
+// 인자에서 MutableState 대신 boolean 값으로 변경합시다.
 @Composable
 fun CheckBoxWithSlotAPI(
     checked: MutableState<Boolean>,
-    content: @Composable () -> Unit
+    content: @Composable RowScope.() -> Unit
 ) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
@@ -87,12 +140,6 @@ fun CheckBoxWithSlotAPI(
         content()
     }
 }
-
-// Step 3: 'content' 의 타입을 '@Composable RowScope.() -> Unit' 로 바꿉시다.
-// 이렇게 다른 콤포저블 컨텐트를 넣는 방식을 Slot API 라 합니다.
-
-// Step 4: 상태를 바꾸는 람다를 '@Composable' 함수의 인자로 뺍시다.
-// 인자에서 MutableState 대신 boolean 값으로 변경합시다.
 
 @Composable
 fun SlotEx() {
@@ -127,7 +174,7 @@ fun SlotEx() {
 //        }
 
         CheckBoxWithSlotAPI(checked1) {
-            Text("텍스트-11")
+            Text("텍스트-11", modifier = Modifier.align(Alignment.CenterVertically))
         }
         CheckBoxWithSlotAPI(checked2) {
             Text("텍스트-22")
