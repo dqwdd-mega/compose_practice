8d00c7146bef024628ba4d856ee2d8dbd2d1d03d
dqwdd-mega <dqwdd@megastudy.net>
2024-06-29 13:21:46 +0900

    feat : Step 2 - ConstraintLayout 생성 후 안에 박스 4개 만들어 놓은 상태, 이제 이걸 옮길 차례다

--- app/src/main/java/com/example/ch03_intro/MainActivity.kt
+++ app/src/main/java/com/example/ch03_intro/MainActivity.kt
@@ -5,39 +5,32 @@ package com.example.ch03_intro
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.Checkbox
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.ch03_intro.ui.theme.Ch04ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintLayout
+import com.example.ch03_intro.ui.theme.ConstraintLayoutTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Ch04ConstraintLayout {
+            ConstraintLayoutTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ConstraintLayoutEx()
+                }
             }
         }
     }
@@ -45,18 +38,37 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun ScaffoldEx() {
+fun ConstraintLayoutEx() {
+    ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+        val (redBox, magentaBox, greenBox, yellowBox) = createRefs()
+
+        Box(
+            modifier = Modifier
+                .size(40.dp)
+                .background(Color.Red)
+        )
+        Box(
+            modifier = Modifier
+                .size(40.dp)
+                .background(Color.Magenta)
+        )
+        Box(
+            modifier = Modifier
+                .size(40.dp)
+                .background(Color.Green)
+        )
+        Box(
+            modifier = Modifier
+                .size(40.dp)
+                .background(Color.Yellow)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun MyPreview() {
-    Ch04ConstraintLayout {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
-        ) {
-            ScaffoldEx()
-        }
+    ConstraintLayoutTheme {
+        ConstraintLayoutEx()
     }
 }
