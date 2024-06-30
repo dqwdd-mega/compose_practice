d37d9b8f0de6256f3b21008ce966389c448794c3
dqwdd-mega <dqwdd@megastudy.net>
2024-06-30 20:50:15 +0900

    feat : 10 DropDownMenu - 드롭다운 세팅

--- app/src/main/java/com/example/ch03_intro/MainActivity.kt
+++ app/src/main/java/com/example/ch03_intro/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -48,6 +50,30 @@ fun DropDownMenuEx() {
             Text(text = "드롭다운 메뉴 열기")
         }
         Text(text = "카운터: $counter")
+    }
+
+    DropdownMenu(
+        expanded = expandDropDownMenu,
+        onDismissRequest = {
+            expandDropDownMenu = false
+        }
+    ) {
+        DropdownMenuItem(
+            text = {
+                Text(text = "증가")
+            },
+            onClick = {
+                counter++
+            }
+        )
+        DropdownMenuItem(
+            text = {
+                Text(text = "감소")
+            },
+            onClick = {
+                counter--
+            }
+        )
     }
 }
 
