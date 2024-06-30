8b3be143923b7725f96b8856a4151563fe7b98e7
dqwdd-mega <dqwdd@megastudy.net>
2024-06-30 20:38:37 +0900

    feat : 10 DropDownMenu - setting

--- app/src/main/java/com/example/ch03_intro/MainActivity.kt
+++ app/src/main/java/com/example/ch03_intro/MainActivity.kt
@@ -6,10 +6,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -20,23 +17,20 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
-import com.example.ch03_intro.ui.theme.CustomDialogTheme
+import com.example.ch03_intro.ui.theme.DropDownMenuTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CustomDialogTheme {
+            DropDownMenuTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    CustomDialogEx()
+                    DropDownMenuEx()
                 }
             }
         }
@@ -45,65 +39,22 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun CustomDialogEx() {
-    var openDialog by remember { mutableStateOf(false) }
+fun DropDownMenuEx() {
+    var expandDropDownMenu by remember { mutableStateOf(false) }
     var counter by remember { mutableStateOf(0) }
 
     Column {
-        Button(
-            onClick = { openDialog = true }
-        ) {
-            Text(text = "다이얼로그 열기")
+        Button(onClick = { expandDropDownMenu = true }) {
+            Text(text = "드롭다운 메뉴 열기")
         }
         Text(text = "카운터: $counter")
-    }
-
-    if (openDialog) {
-        Dialog(
-            onDismissRequest = {
-                openDialog = false
-            }
-        ) {
-            Surface {
-                Column(modifier = Modifier.padding(8.dp)) {
-                    Text(text = "버튼을 클릭해주세요\n +1을 누르면 값이 증가됩니다\n -1을 누르면 값이 감소합니다")
-                    Row(
-                        modifier = Modifier.align(Alignment.End)
-                    ) {
-                        Button(
-                            onClick = {
-                                openDialog = false
-                            }
-                        ) {
-                            Text(text = "취소")
-                        }
-                        Button(
-                            onClick = {
-                                openDialog = false
-                                counter++
-                            }
-                        ) {
-                            Text(text = "+1")
-                        }
-                        Button(
-                            onClick = {
-                                openDialog = false
-                                counter--
-                            }
-                        ) {
-                            Text(text = "-1")
-                        }
-                    }
-                }
-            }
-        }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun MyPreview() {
-    CustomDialogTheme {
-        CustomDialogEx()
+    DropDownMenuTheme {
+        DropDownMenuEx()
     }
 }
