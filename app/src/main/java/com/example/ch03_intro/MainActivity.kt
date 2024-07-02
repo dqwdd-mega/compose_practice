87c08a3125cb1c06be1119ea5efa13a234fa3023
dqwdd-mega <dqwdd@megastudy.net>
2024-07-02 21:23:32 +0900

    feat : 11 SnackBar - snackbarResult 설정

--- app/src/main/java/com/example/ch03_intro/MainActivity.kt
+++ app/src/main/java/com/example/ch03_intro/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -72,10 +73,15 @@ fun SnackBarEx() {
                 Button(onClick = {
                     counter++
                     coroutineScope.launch {
-                        snackbarHostState.showSnackbar(
+                        val snackbarResult = snackbarHostState.showSnackbar(
                             message = "Counter: $counter",
                             duration = SnackbarDuration.Short
                         )
+
+                        when (snackbarResult) {
+                            SnackbarResult.Dismissed -> {}
+                            SnackbarResult.ActionPerformed -> {}
+                        }
                     }
                 }) {
                     Text("Increase Counter")
