fb394ab7c4b8a16c41eddf1e63e1ac5d08d90f95
dqwdd-mega <dqwdd@megastudy.net>
2024-08-04 18:18:38 +0900

    feat : 12 BottomAppBar - 클릭 시 counter 숫자 변경 및 토스트

--- app/src/main/java/com/example/ch03_intro/MainActivity.kt
+++ app/src/main/java/com/example/ch03_intro/MainActivity.kt
@@ -1,29 +1,23 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
-
 package com.example.ch03_intro
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
@@ -54,6 +48,7 @@ class MainActivity : ComponentActivity() {
 fun BottomAppBarEx() {
     val snackBarHostState = remember { SnackbarHostState() }
     val coroutineScope = rememberCoroutineScope()
+    var counter by remember { mutableIntStateOf(0) }
 
     Scaffold(
         snackbarHost = { SnackbarHost(snackBarHostState) },
@@ -69,13 +64,39 @@ fun BottomAppBarEx() {
                 ) {
                     Text(text = "인사하기")
                 }
+                Button(
+                    onClick = {
+                        counter++
+                        coroutineScope.launch {
+                            snackBarHostState.showSnackbar("${counter} 입니다")
+                        }
+                    }
+                ) {
+                    Text(text = "Plus")
+                }
+                Button(
+                    onClick = {
+                        counter--
+                        coroutineScope.launch {
+                            snackBarHostState.showSnackbar("${counter} 입니다")
+                        }
+                    }
+                ) {
+                    Text(text = "Minus")
+                }
             }
         }
-    ) {
-        Text(
-            text = "Scaffold content",
-            modifier = Modifier.padding(it)
-        )
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+        ) {
+            Text(
+                text = "카운터는 ${counter}회입니다",
+                modifier = Modifier.align(Alignment.Center)
+            )
+        }
     }
 }
 
